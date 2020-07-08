88c9a0969880cef03003ca3660c6151965e4e3ee
Konstantin Strümpf <k.struempf@icloud.com>
2020-07-08 19:05:14 +0200

    Added second sensor to controller

--- DeviceManager/sensor-controller.py
+++ DeviceManager/sensor-controller.py
@@ -9,26 +9,33 @@ except ImportError:
 
 class Program:
     _running = True
-    _sensor = None
+    _sensor1 = None
+    _sensor2 = None
 
     def __init__(self):
-        self._sensor = BinarySensor(26, 19)
+        self._sensor1 = BinarySensor(20, 16)
+        self._sensor2 = BinarySensor(26, 19)
 
     def __del__(self):
-        del self._sensor
+        del self._sensor1
 
     def run(self):
         time.sleep(1)
         print("Running...")
         while self._running:
-            self._sensor.activate()
+            self._sensor1.activate()
+            self._sensor2.activate()
             time.sleep(1)
 
-            value = self._sensor.read()
-            print("Read value " + str(value))
+            value = self._sensor1.read()
+            print("Read value " + str(value) + " on sensor 1")
+            value = self._sensor2.read()
+            print("Read value " + str(value) + " on sensor 2")
+
             time.sleep(1)
 
-            self._sensor.deactivate()
+            self._sensor1.deactivate()
+            self._sensor2.deactivate()
             time.sleep(4)
 
     def stop(self):
